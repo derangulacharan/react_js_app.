6b0c76180ed091e5ba4140a2e4f430ec1f759a6b
derangulacharan <derangulacharan64@gmail.com>
2023-10-12 14:10:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import React from 'react';
+import WordCounter from './components/WordCounter';
+import './App.css'; // Import the CSS file
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Responsive paragraph word counter </h1>
+      <WordCounter />
     </div>
   );
 }
